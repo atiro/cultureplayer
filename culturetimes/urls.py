b318633a711cc3d4d43538c476bf9c5b54add91e
Richard Palmer <richard@tiro.org.uk>
2014-12-01 21:31:20 +0000

    Working version!
    	Displays quite nicely in tabular form
    	Header a bit messy, to fix after more data imported

--- culturetimes/urls.py
+++ culturetimes/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, url
-from culturetimes.views import InstitutionView, LocationView, ObjectView
+from culturetimes.views import InstitutionView, LocationView, ObjectView2
 
 urlpatterns = patterns('',
 	url(r'^$', InstitutionView.as_view(), name="institutions"),
-	url(r'^(?P<institution>\d+)/(?P<location>\d+)', ObjectView.as_view(), name="objects"),
+	url(r'^(?P<institution>\d+)/(?P<location>\d+)$', ObjectView2, name="objects2"),
 	url(r'^(?P<institution>\d+)/', LocationView.as_view(), name="locations"),
 )
--- culturetimes/views.py
+++ culturetimes/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.views.generic.list import ListView
+from django.template import RequestContext
 from culture.models import Institution, Location, Object
 from genome.models import Programme
+from datetime import datetime
+
 
 # Create your views here.
 
@@ -40,3 +43,34 @@ class ObjectView(ListView):
 #def programmes(request, institution, location):
 #	try:
 ##		objects
+
+def ObjectView2(request, institution, location):
+
+#	# Order 
+	# Create dict indexed by object 
+	# obj[id][decade][
+	
+	loc = Location.objects.get(id=location)
+	objs = Object.objects.filter(location=location)
+
+	for obj in objs:
+	  programmes = []
+	  year = 1920
+	  while year < 1990:
+	    decade_start = datetime(year,1,1)
+	    decade_end = datetime(year + 10,1,1)
+
+	    obj_programmes = obj.programme_set.filter(datetime__range=([decade_start, decade_end]))
+	    if len(obj_programmes) > 0:
+		programmes.append(obj_programmes[0])
+	    else:
+		programmes.append(None)
+
+	    year += 10
+
+	  obj.programmes = programmes
+	   
+	return render_to_response("culturetimes/objects.html",
+				 {"object_list": objs,
+				  "location": loc},
+				 context_instance=RequestContext(request))
